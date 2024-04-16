df6aea0b2a73a1ba28e35f13c04555149e9d4398
Daria Shnyrkova <57520792+DezertRose@users.noreply.github.com>
2024-04-16 06:35:26 +0300

    Lr 3 full

---
+++ hub/edge/app/usecases/data_processing.py
@@ -0,0 +1,16 @@
+from app.entities.agent_data import AgentData
+from app.entities.processed_agent_data import ProcessedAgentData
+
+
+def process_agent_data(
+    agent_data: AgentData,
+) -> ProcessedAgentData:
+    """
+    Process agent data and classify the state of the road surface.
+    Parameters:
+        agent_data (AgentData): Agent data that containing accelerometer, GPS, and timestamp.
+    Returns:
+        processed_data_batch (ProcessedAgentData): Processed data containing the classified state of the road surface and agent data.
+    """
+    road_state = "bad" if 20 > agent_data.accelerometer.z > 10 else "very bad"
+    return ProcessedAgentData(road_state=road_state, agent_data=agent_data)
